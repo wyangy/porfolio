d2b8c0a1ddd6a45fe64aa2fd45bb78177b7a131b
Wendy <wendyyang@developersinstitute.co.nz>
2022-06-11 22:09:31 +1200

    add images

--- wyangynz/src/components/Project.js
+++ wyangynz/src/components/Project.js
@@ -1,10 +1,12 @@
 // const Project = ({ title, image, description, technology, url }) => {
 const Project = ({ project }) => {
 	const { title, image, description, technology, url } = project;
+
+	console.log(image);
 	return (
 		<li className="project">
 			<h3>{title}</h3>
-			<img src={image} alt={title} />
+			<img src={"images/" + image} alt={title} />
 			<p>{description}</p>
 			<span>Technology: {technology.join(", ") + "."}</span>
 			<button onClick={() => window.open({ url }, "_blank")}>
