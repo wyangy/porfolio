05b5bde18a0ab8dd909128f56204d88b9c582763
Wendy <wendyyang@developersinstitute.co.nz>
2022-06-11 21:36:12 +1200

    add projects

--- wyangynz/src/components/Projects.js
+++ wyangynz/src/components/Projects.js
@@ -4,7 +4,7 @@ const Article = ({ title, image, description, technology, url }) => {
 			<h3>{title}</h3>
 			<img src={image} alt={title} />
 			<p>{description}</p>
-
+			<span>Technology: {technology.join(", ") + "."}</span>
 			<button onClick={() => window.open({ url }, "_blank")}>
 				view website
 			</button>
