35f432c07a56d6256f44a06fc7a339bc48be5da2
Wendy <wyangynz@gmail.com>
2022-06-14 12:41:04 +1200

    adjust heading

--- wyangynz/src/components/Project.js
+++ wyangynz/src/components/Project.js
@@ -5,7 +5,7 @@ const StyledListItem = styled.li`
 		linear-gradient(-150deg, #212121 15px, snow 0) top right,
 		linear-gradient(-30deg, #212121 15px, snow 0) bottom right,
 		linear-gradient(30deg, #212121 15px, snow 0) bottom left;
-	background-size: 50% 50%;
+	background-size: 60% 60%;
 	background-repeat: no-repeat;
 
 	display: flex;
@@ -29,96 +29,93 @@ const StyledListItem = styled.li`
 		gap: 1rem;
 
 		font-size: 18px;
-		/* font-weight: 400; */
 	}
 
 	.heading {
 		display: grid;
-		grid-template-columns: auto min-content max-content min-content auto;
-		/* border-top: 2px solid #212121;
-		width: 80%; */
+		grid-template-columns: auto min-content min-content max-content min-content min-content auto;
 		margin: 0 auto 2.5rem;
-		/* margin: 1rem 3rem 2rem; */
-		/* margin-bottom: 2rem; */
 	}
 
 	h3 {
-		background: #212121;
-		color: white;
 		display: flex;
-		/* min-width: 20rem; */
-		/* width: max-content; */
+		justify-content: center;
+		align-items: center;
+
 		min-width: 13rem;
-		/* height: 3rem; */
-		/* padding: 1rem 3rem 1rem; */
 		padding: 0.7rem 3rem 0.7rem;
 		border-left: 2px solid snow;
 		border-right: 2px solid snow;
 
+		background: #212121;
+		color: white;
 		text-transform: uppercase;
-		/* align-self: center;
-		align-content: center; */
-		justify-content: center;
-		align-items: center;
-		/* text-align: center; */
 		letter-spacing: 0.3rem;
 		word-spacing: 0.3rem;
 		font-size: 24px;
-		font-weight: 300;
+		font-weight: 400;
 	}
 
-	.left-design-inner {
+	.heading-left-inner {
 		background: #212121;
-		height: 2rem;
+		height: 2.3rem;
 		width: 0.5em;
 		justify-self: right;
-		border-left: 1px solid snow;
+		border-left: 2px solid snow;
 	}
 
-	.left-design-outer {
+	.heading-left-outer {
 		background: #212121;
-		height: 1rem;
-		width: 0.3rem;
+		height: 1.4rem;
+		width: 0.4rem;
+		justify-self: right;
+		border-left: 2px solid snow;
+	}
+
+	.heading-left-edge {
+		background: #212121;
+		height: 0.6rem;
+		width: 0.2rem;
 		justify-self: right;
 	}
 
-	.right-design-inner {
+	.heading-right-inner {
 		background: #212121;
-		height: 2rem;
+		height: 2.3rem;
 		width: 0.5em;
-		border-right: 1px solid snow;
+		border-right: 2px solid snow;
 	}
 
-	.right-design-outer {
+	.heading-right-outer {
 		background: #212121;
-		height: 1rem;
-		width: 0.3rem;
+		height: 1.4rem;
+		width: 0.4rem;
+		border-right: 2px solid snow;
+	}
+
+	.heading-right-edge {
+		background: #212121;
+		height: 0.6rem;
+		width: 0.2rem;
 	}
 
 	img {
 		max-width: 40rem;
 		width: 100%;
-		/* margin: 0 auto;
-		display: block;
-		padding: 0 1rem; */
 		margin-bottom: 1rem;
+		border-radius: 0.5rem;
 	}
 
 	p {
-		/* margin-bottom: 1.5rem; */
-		/* padding: 0 3rem; */
 		line-height: 1.4;
 	}
 
 	span {
-		/* width: 100%; */
-		/* margin-bottom: 0.5rem; */
-		/* padding: 0 3rem; */
 		font-style: italic;
 	}
 
 	button {
-		margin: 2rem auto 0;
+		margin: 1.5rem auto 0;
 		padding: 1rem;
 		width: 15rem;
 		color: white;
@@ -128,12 +125,11 @@ const StyledListItem = styled.li`
 		word-spacing: 0.2rem;
 
 		border-style: none;
-
 		background: linear-gradient(135deg, snow 7px, #212121 0) top left,
 			linear-gradient(-135deg, snow 7px, #212121 0) top right,
 			linear-gradient(-45deg, snow 7px, #212121 0) bottom right,
 			linear-gradient(45deg, snow 7px, #212121 0) bottom left;
-		background-size: 50% 50%;
+		background-size: 60% 60%;
 		background-repeat: no-repeat;
 	}
 `;
@@ -145,11 +141,13 @@ const Project = ({ project }) => {
 		<StyledListItem>
 			<div className="border">
 				<div className="heading">
-					<div className="left-design-outer"></div>
-					<div className="left-design-inner"></div>
+					<div className="heading-left-edge" />
+					<div className="heading-left-outer" />
+					<div className="heading-left-inner" />
 					<h3>{title}</h3>
-					<div className="right-design-inner"></div>
-					<div className="right-design-outer"></div>
+					<div className="heading-right-inner" />
+					<div className="heading-right-outer" />
+					<div className="heading-right-edge" />
 				</div>
 				<div className="content">
 					<img src={require(`../images/${image}`)} alt={title} />
