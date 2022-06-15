a32a20a21f8e071684d8035a28cc7923abc00909
Wendy <wyangynz@gmail.com>
2022-06-15 16:57:11 +1200

    add footer design

--- wyangynz/src/components/Footer.js
+++ wyangynz/src/components/Footer.js
@@ -1,9 +1,110 @@
 import styled from "styled-components";
+import FooterDesign from "./FooterDesign";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-const StyledFooter = styled.footer``;
+const StyledFooter = styled.header`
+	.content {
+		display: grid;
+		justify-items: center;
+		grid-gap: 1rem;
+
+		padding: 3rem 0 2rem;
+		margin-bottom: 3rem;
+
+		@media (min-width: 600px) {
+			padding: 4rem 0 2rem;
+			margin-bottom: 4rem;
+		}
+
+		@media (min-width: 1024px) {
+			padding: 5rem 0 3rem;
+			margin-bottom: 5rem;
+		}
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 36px;
+		font-weight: 200;
+		letter-spacing: 0.5rem;
+
+		@media (min-width: 600px) {
+			font-size: 60px;
+			letter-spacing: 0.7rem;
+		}
+
+		@media (min-width: 800px) {
+			font-size: 80px;
+		}
+
+		@media (min-width: 1024px) {
+			font-size: 100px;
+			font-weight: 100;
+			letter-spacing: 0.9rem;
+		}
+
+		@media (min-width: 1280px) {
+			font-size: 110px;
+		}
+	}
+
+	p {
+		font-style: italic;
+
+		@media (min-width: 600px) {
+			font-size: 20px;
+		}
+	}
+
+	.icons {
+		display: flex;
+		gap: 1rem;
+		font-size: 22px;
+
+		@media (min-width: 600px) {
+			font-size: 30px;
+		}
+	}
+
+	.icons a {
+		color: #212121;
+	}
+`;
 
 const Footer = () => {
-	return <StyledFooter></StyledFooter>;
+	return (
+		<StyledFooter>
+			<FooterDesign>
+				<div className="content">
+					<h1>Wendy Yang</h1>
+					<p>Full-stack Web Developer</p>
+					<div className="icons">
+						<a
+							title="link to LinkedIn"
+							href="https://www.linkedin.com/in/wyangynz/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={faLinkedin} />
+						</a>
+						<a
+							title="link to GitHub"
+							href="https://github.com/wyangy"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={faGithub} />
+						</a>
+						<a href="mailto: wyangynz@gmail.com">
+							<FontAwesomeIcon icon={faEnvelope} />
+						</a>
+					</div>
+				</div>
+			</FooterDesign>
+		</StyledFooter>
+	);
 };
 
 export default Footer;
--- wyangynz/src/components/FooterDesign.js
+++ wyangynz/src/components/FooterDesign.js
@@ -1,9 +1,109 @@
 import styled from "styled-components";
 
-const StyledFooterDesign = styled.div``;
+const StyledFooterDesign = styled.div`
+	.border1 {
+		background: snow;
+		/* clip-path: polygon(0% 0%, 100% 0%, 100% 60%, 81% 100%, 19% 100%, 0% 60%); */
+		/* background: magenta; */
+		/* clip-path: polygon(0% 0%, 100% 0%, 100% 60%, 81% 100%, 19% 100%, 0% 60%); */
+		clip-path: polygon(100% 100%, 0% 100%, 0% 40%, 19% 0%, 81% 0%, 100% 40%);
+	}
 
-const FooterDesign = () => {
-	return <StyledFooterDesign></StyledFooterDesign>;
+	.border2 {
+		background: #212121;
+		/* clip-path: polygon(
+			0% 0%,
+			100% 0%,
+			100% 60.7%,
+			81.1% 100%,
+			18.9% 100%,
+			0% 60.7%
+		); */
+		clip-path: polygon(
+			100% 100%,
+			0% 100%,
+			0% 39.3%,
+			18.9% 0%,
+			81.1% 0%,
+			100% 39.3%
+		);
+	}
+
+	.border3 {
+		background: snow;
+		/* clip-path: polygon(
+			0% 0%,
+			100% 0%,
+			100% 64.9%,
+			81.9% 100%,
+			18.1% 100%,
+			0% 64.9%
+		); */
+		clip-path: polygon(
+			100% 100%,
+			0% 100%,
+			0% 35.1%,
+			18.1% 0%,
+			81.9% 0%,
+			100% 35.1%
+		);
+	}
+
+	.border4 {
+		background: #212121;
+		/* clip-path: polygon(
+			0% 0%,
+			100% 0%,
+			100% 65.5%,
+			82% 100%,
+			18% 100%,
+			0% 65.5%
+		); */
+		clip-path: polygon(
+			100% 100%,
+			0% 100%,
+			0% 34.5%,
+			18% 0%,
+			82% 0%,
+			100% 34.5%
+		);
+	}
+
+	.border5 {
+		background: snow;
+		/* clip-path: polygon(
+			0% 0%,
+			100% 0%,
+			100% 68%,
+			82.7% 100%,
+			17.3% 100%,
+			0% 68%
+		); */
+		clip-path: polygon(
+			100% 100%,
+			0% 100%,
+			0% 32%,
+			17.3% 0%,
+			82.7% 0%,
+			100% 32%
+		);
+	}
+`;
+
+const FooterDesign = ({ children }) => {
+	return (
+		<StyledFooterDesign>
+			<div className="border5">
+				<div className="border4">
+					<div className="border3">
+						<div className="border2">
+							<div className="border1">{children}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</StyledFooterDesign>
+	);
 };
 
 export default FooterDesign;
